884725f7b63a82849b026ecec4c4f836b6ae9dec
Luke Riley <lbriley11@gmail.com>
2026-02-01 23:45:31 -0500

    commit 1

---
+++ AUList_source/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "AUList.h"
+
+int main(int argc, char** argv) {	
+	AUList TestList;
+	std::cout<<"Newly Created List: ";
+	TestList.PrintList();
+	for (int i=0; i<10; i++)
+	  TestList.PutItem(100-i*10);
+	std::cout<<"List after 'PutItem' (100 down to 10) calls: ";
+	TestList.PrintList();
+	std::cout<<"Length after 'PutItem' calls: " <<TestList.GetLength() <<std::endl;
+	std::cout<<"IsFull after 'PutItem' calls? " <<TestList.IsFull() <<std::endl;
+	TestList.DeleteItem(50);
+	std::cout<<"List after 'DeleteItem' (50) call: ";
+	TestList.PrintList();
+	std::cout<<"IsFull after 'DeleteItem' call? " <<TestList.IsFull() <<std::endl;
+	std::cout<<"Index of value 80: "<<TestList.GetItem(80)<<std::endl;
+	std::cout<<"Index of value 25: "<<TestList.GetItem(25)<<std::endl;
+	TestList.MakeEmpty();
+	std::cout<<"List after 'MakeEmpty': ";
+	std::cout<<"The Minimum Value is: "<<TestList.GetMin()<<std::endl;
+	TestList.PrintList();		
+	return 0;
+}
